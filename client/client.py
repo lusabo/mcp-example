0ff3173c58aac2f3add7fa11c16fda0ebceef5fb
Luciano Borges <lucianosantosborges@gmail.com>
2025-05-25 19:28:56 +0100

    Criando o client com Streamlit

--- client/client.py
+++ client/client.py
@@ -1,9 +1,20 @@
+import streamlit as st
 import asyncio
 from fastmcp import Client
 
-async def main():
+async def call_hello(name):
     async with Client("http://localhost:8000/mcp") as client:
-        response = await client.call_tool("hello", {"name": "World"})
-        print(response[0].text)
+        response = await client.call_tool("hello", {"name": name})
+        return response[0].text
 
-asyncio.run(main())
+st.title("Olá com MCP 👋")
+
+name_input = st.text_input("Digite seu nome:")
+
+if st.button("Enviar"):
+    if name_input.strip():
+        with st.spinner("Chamando o servidor..."):
+            result = asyncio.run(call_hello(name_input))
+            st.success(result)
+    else:
+        st.warning("Por favor, insira um nome.")
